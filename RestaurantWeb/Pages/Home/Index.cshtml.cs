20d1bc583a6ba7dabf5a8321721dacc8769baf02
Сел Тим <tima.sele@gmail.com>
2025-06-03 16:21:38 +0300

    fix 16:21

--- RestaurantWeb/Pages/Home/Index.cshtml.cs
+++ RestaurantWeb/Pages/Home/Index.cshtml.cs
@@ -1,12 +1,16 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Security.Claims;
 
 namespace RestaurantWeb.Pages.Home
 {
     public class IndexModel : PageModel
     {
+        public string Roles { get; set; }
         public void OnGet()
         {
+            Roles = string.Join(", ", User.Claims.Where(c=>c.Type == ClaimTypes.Role).Select(c=>c.Value));
+
         }
     }
 }
